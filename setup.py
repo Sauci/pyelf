31b887458584303b3e6cbdcc19d0000bf1b3b550
lmbsog0 <guillaume.sottas@liebherr.com>
2021-11-10 14:57:34 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyelf',
-    version='0.1.0',
+    version='0.1.1',
     packages=['pyelf'],
     url='https://github.com/Sauci/pyelf',
     license='BSD',
